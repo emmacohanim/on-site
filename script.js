bba074e2133c69da3e5d96d0c68a41c976186f91
Emma Cohanim <emmacohanim@gmail.com>
2022-07-13 15:31:47 -0700

    project complete

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const input = document.querySelector('.aboutme')
+function showDescription() {
+        let hidden1 = document.querySelector('#small1');
+        if (hidden1.style.display === "none") {
+    hidden1.style.display = "block";
+        } else {
+    hidden1.style.display = "none";
+     }
+    }
+input.addEventListener('click', showDescription);
+
+const input2 = document.querySelector('.contact')
+function showDescription2() {
+        let hidden2 = document.querySelector('#small2');
+        if (hidden2.style.display === "none") {
+    hidden2.style.display = "block";
+        } else {
+    hidden2.style.display = "none";
+     }
+    }
+input2.addEventListener('click', showDescription2);
